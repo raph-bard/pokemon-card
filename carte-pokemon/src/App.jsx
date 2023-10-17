616fd757856554f2b11374e5eb14192e5c263fee
raph-bard <raphael.bard.pro@googlemail.com>
2023-10-17 22:42:02 +0200

    début des props

--- carte-pokemon/src/App.jsx
+++ carte-pokemon/src/App.jsx
@@ -1,19 +1,38 @@
-import './App.css'
+import './App.css';
 import StyledComponent from './components/styledComponent'
 
 
-// const pokemonCard = [
-//   {
-//     id: "salameche",
-//     pkmName: "Salamèche",
-//     pkmEvo: 0,
-//   },
-// ]
+const pokemonCard = [
+  {
+    id: "salameche",
+    pkmName: "Salamèche",
+    pkmEvo: null,
+    pv: 50,
+  },
+  {
+    id: "reptincel",
+    pkmName: "Reptincel",
+    pkmEvo: "Salamèche",
+    pv: 80,
+  },
+  {
+    id: "dracaufeu",
+    pkmName: "Dracaufeu",
+    pkmEvo: "Reptincel",
+    pv: 120,
+  }
+]
 
 function App() {
 
   return (
-    <StyledComponent />
+    <div className='d-flex'>
+      {pokemonCard.map((pokemon) => (
+        <StyledComponent key={pokemon.id} pkmId={pokemon.id} pkmName={pokemon.pkmName} pkmEvo={pokemon.pkmEvo} pv={pokemon.pv}/>
+      ))};
+
+
+    </div>
   )
  
 
--- carte-pokemon/src/components/styledComponent.jsx
+++ carte-pokemon/src/components/styledComponent.jsx
@@ -8,35 +8,38 @@ const Container = styled.div`
   border-radius: 14px;
 `;
 
-
-
-
-const StyledComponents = () => (
-  <Container className='pokemon-card'>
-    <h1 className='salameche base-pokemon'>Salamèche<i className='nrj nrj-feu'></i><span>50 PV</span></h1>
-    <figure>
-        <img className='pokemon-image' src='src/assets/salameche.jpg' alt='Salamèche'/>
+const StyledComponents = (pokemon) => {
+  return (
+    <Container className='pokemon-card'>
+      <h1 className={`${pokemon.pkmId}`}>
+        <b>{pokemon.pkmEvo === null ? "Pokemon de base" : `Evolution de ${pokemon.pkmEvo}`}</b>{pokemon.pkmName}
+        <i className='nrj nrj-feu'></i>
+        <span>{pokemon.pv} PV</span>
+      </h1>
+      <figure>
+        <img className='pokemon-image' src={`src/assets/${pokemon.pkmId}.jpg`} alt={pokemon.pkmName}/>
         <figcaption>Pokémon Lézard. Taille: 0.6 m, poids: 8.5 kg</figcaption>
-    </figure>
-    <div className='attack-block'>
+      </figure>
+      <div className='attack-block'>
         <div className='attack attack-1 attack-only'>
-            <div className='nrj-block'><i className='nrj nrj-normal'></i></div>
-            <h2>Griffe</h2>
-            <span>10</span>
+          <div className='nrj-block'><i className='nrj nrj-normal'></i></div>
+          <h2>Griffe</h2>
+          <span>10</span>
         </div>
         <div className='attack attack-2'>
-            <div className='nrj-block'><i className='nrj nrj-feu'></i><i className='nrj nrj-normal'></i></div>
-            <h2>Flammèche <p>Défaussez 1 carte énergie <i className='nrj nrj-feu'></i> attachée à Salamèche pour pouvoir utiliser cette attaque.</p></h2>
-            <span>30</span>
+          <div className='nrj-block'><i className='nrj nrj-feu'></i><i className='nrj nrj-normal'></i></div>
+          <h2>Flammèche <p>Défaussez 1 carte énergie <i className='nrj nrj-feu'></i> attachée à Salamèche pour pouvoir utiliser cette attaque.</p></h2>
+          <span>30</span>
         </div>
-    </div>
-    <div className='stats-block'>
+      </div>
+      <div className='stats-block'>
         <h3>Faiblesse<i className='nrj nrj-eau'></i></h3>
         <h3>Résistance</h3>
         <h3>Coût de retraite<i className='nrj nrj-normal'></i></h3>
-    </div>
-    <p className='infos-block'>Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme autour de sa queue.   NIV.10   No.4</p>
-  </Container> 
-);
+      </div>
+      <p className='infos-block'>Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme autour de sa queue.   NIV.10   No.4</p>
+    </Container>
+  );
+};
 
-export default StyledComponents
+export default StyledComponents;
