88830fd3f52670f1ebd7d166dce2f272f7884ae3
raph-bard <raphael.bard.pro@googlemail.com>
2023-10-18 22:42:17 +0200

    dynamisation des couleurs de fond des cartes, le type et les faiblesses  en fonction du type

--- carte-pokemon/src/App.jsx
+++ carte-pokemon/src/App.jsx
@@ -2,40 +2,38 @@ import { useState } from "react";
 import "./App.css";
 import StyledComponent from "./components/styledComponent";
 
-class Pokemon {
-  id;
-  pkmName;
-  pkmEvo;
-  pv;
-  figDescr;
-  figHeight;
-  figWeight;
+const pokemonProperties = [
+  "id",
+  "name",
+  "evo",
+  "type",
+  "pv",
+  "figDescr",
+  "figHeight",
+  "figWeight",
+];
 
-  constructor(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight) {
-    this.id = id;
-    this.pkmName = pkmName;
-    this.pkmEvo = pkmEvo;
-    this.pv = pv;
-    this.figDescr = figDescr;
-    this.figHeight = figHeight;
-    this.figWeight = figWeight;
+class Pokemon {
+  constructor(...args) {
+    for (let i = 0; i < args.length; i++) {
+      this[pokemonProperties[i]] = args[i];
+    }
   }
 }
 
 class StarterFeu extends Pokemon {
-  
-  constructor(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight) {
-    super(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight);
+  constructor(...args) {
+    super(...args);
   }
 }
 class StarterEau extends Pokemon {
-  constructor(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight) {
-    super(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight);
+  constructor(...args) {
+    super(...args);
   }
 }
 class StarterPlante extends Pokemon {
-  constructor(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight) {
-    super(id, pkmName, pkmEvo, pv, figDescr, figHeight, figWeight);
+  constructor(...args) {
+    super(...args);
   }
 }
 
@@ -44,23 +42,96 @@ const pokemonCardsFeu = [
     "salameche",
     "Salamèche",
     null,
+    "feu",
     50,
     "Pokémon Lézard",
     0.6,
     8.5
   ),
-  new StarterFeu("reptincel", "Reptincel", "Reptincel", 80, "Pokémon Flamme", 1.1, 19),
-  new StarterFeu("dracaufeu", "Dracaufeu", "Salamèche", 120, "Pokémon Flamme", 1.7, 90.5),
+  new StarterFeu(
+    "reptincel",
+    "Reptincel",
+    "Reptincel",
+    "feu",
+    80,
+    "Pokémon Flamme",
+    1.1,
+    19
+  ),
+  new StarterFeu(
+    "dracaufeu",
+    "Dracaufeu",
+    "Salamèche",
+    "feu",
+    120,
+    "Pokémon Flamme",
+    1.7,
+    90.5
+  ),
 ];
 const pokemonCardsEau = [
-  new StarterEau("carapuce", "Carapuce", null, 40, "Pokémon MiniTortue", 0.5, 9),
-  new StarterEau("carabaffe", "Carabaffe", "Carapuce", 70, "Pokémon Tortue", 1, 22.5),
-  new StarterEau("tortank", "Tortank", "Carabaffe", 100, "Pokémon Carapace", 1.6, 85.5),
+  new StarterEau(
+    "carapuce",
+    "Carapuce",
+    null,
+    "eau",
+    40,
+    "Pokémon MiniTortue",
+    0.5,
+    9
+  ),
+  new StarterEau(
+    "carabaffe",
+    "Carabaffe",
+    "Carapuce",
+    "eau",
+    70,
+    "Pokémon Tortue",
+    1,
+    22.5
+  ),
+  new StarterEau(
+    "tortank",
+    "Tortank",
+    "Carabaffe",
+    "eau",
+    100,
+    "Pokémon Carapace",
+    1.6,
+    85.5
+  ),
 ];
 const pokemonCardsPlante = [
-  new StarterPlante("bulbizarre", "Bulbizarre", null, 40, "Pokémon Graine", 0.7, 6.9),
-  new StarterPlante("herbizarre", "Herbizarre", "Bulbizarre", 70, "Pokémon Graine", 1.0, 13.0),
-  new StarterPlante("florizarre", "Florizarre", "Herbizarre", 100, "Pokémon Graine", 2.0, 100),
+  new StarterPlante(
+    "bulbizarre",
+    "Bulbizarre",
+    null,
+    "plante",
+    40,
+    "Pokémon Graine",
+    0.7,
+    6.9
+  ),
+  new StarterPlante(
+    "herbizarre",
+    "Herbizarre",
+    "Bulbizarre",
+    "plante",
+    70,
+    "Pokémon Graine",
+    1.0,
+    13.0
+  ),
+  new StarterPlante(
+    "florizarre",
+    "Florizarre",
+    "Herbizarre",
+    "plante",
+    100,
+    "Pokémon Graine",
+    2.0,
+    100
+  ),
 ];
 
 function App() {
@@ -82,13 +153,7 @@ function App() {
         {pokemonCard.map((pokemon) => (
           <StyledComponent
             key={pokemon.id}
-            pkmId={pokemon.id}
-            pkmName={pokemon.pkmName}
-            pkmEvo={pokemon.pkmEvo}
-            pv={pokemon.pv}
-            figDescr={pokemon.figDescr}
-            figHeight={pokemon.figHeight}
-            figWeight={pokemon.figWeight}
+            {...pokemon}
           />
         ))}
       </div>
--- carte-pokemon/src/components/styledComponent.jsx
+++ carte-pokemon/src/components/styledComponent.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
   width: 358px;
@@ -9,34 +9,76 @@ const Container = styled.div`
 
 const StyledComponents = (pokemon) => {
   return (
-    <Container className='pokemon-card'>
-      <h1 className={`${pokemon.pkmId}`}>
-        <b>{pokemon.pkmEvo === null ? "Pokemon de base" : `Evolution de ${pokemon.pkmEvo}`}</b>{pokemon.pkmName}
-        <i className='nrj nrj-feu'></i>
+    <Container className={`pokemon-card type-${pokemon.type}`}>
+      <h1 className={`${pokemon.id}`}>
+        <b>
+          {pokemon.evo === null
+            ? "Pokemon de base"
+            : `Evolution de ${pokemon.evo}`}
+        </b>
+        {pokemon.name}
+        <i className={`nrj nrj-${pokemon.type}`}></i>
         <span>{pokemon.pv} PV</span>
       </h1>
       <figure>
-        <img className='pokemon-image' src={`src/assets/${pokemon.pkmId}.jpg`} alt={pokemon.pkmName}/>
-        <figcaption>{pokemon.figDescr}. Taille: {pokemon.figHeight} m, poids: {pokemon.figWeight} kg</figcaption>
+        <img
+          className="pokemon-image"
+          src={`src/assets/${pokemon.id}.jpg`}
+          alt={pokemon.pkmName}
+        />
+        <figcaption>
+          {pokemon.figDescr}. Taille: {pokemon.figHeight} m, poids:{" "}
+          {pokemon.figWeight} kg
+        </figcaption>
       </figure>
-      <div className='attack-block'>
-        <div className='attack attack-1 attack-only'>
-          <div className='nrj-block'><i className='nrj nrj-normal'></i></div>
+      <div className="attack-block">
+        <div className="attack attack-1 attack-only">
+          <div className="nrj-block">
+            <i className="nrj nrj-normal"></i>
+          </div>
           <h2>Griffe</h2>
           <span>10</span>
         </div>
-        <div className='attack attack-2'>
-          <div className='nrj-block'><i className='nrj nrj-feu'></i><i className='nrj nrj-normal'></i></div>
-          <h2>Flammèche <p>Défaussez 1 carte énergie <i className='nrj nrj-feu'></i> attachée à Salamèche pour pouvoir utiliser cette attaque.</p></h2>
+        <div className="attack attack-2">
+          <div className="nrj-block">
+            <i className="nrj nrj-feu"></i>
+            <i className="nrj nrj-normal"></i>
+          </div>
+          <h2>
+            Flammèche{" "}
+            <p>
+              Défaussez 1 carte énergie <i className="nrj nrj-feu"></i> attachée
+              à Salamèche pour pouvoir utiliser cette attaque.
+            </p>
+          </h2>
           <span>30</span>
         </div>
       </div>
-      <div className='stats-block'>
-        <h3>Faiblesse<i className='nrj nrj-eau'></i></h3>
+      <div className="stats-block">
+        <h3>
+          Faiblesse
+          <i
+            className={`nrj nrj-${
+              pokemon.type === "feu"
+                ? "eau"
+                : pokemon.type === "eau"
+                ? "plante"
+                : pokemon.type === "plante"
+                ? "feu"
+                : "autreType"
+            }`}
+          ></i>
+        </h3>
+
         <h3>Résistance</h3>
-        <h3>Coût de retraite<i className='nrj nrj-normal'></i></h3>
+        <h3>
+          Coût de retraite<i className="nrj nrj-normal"></i>
+        </h3>
       </div>
-      <p className='infos-block'>Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme autour de sa queue.   NIV.10   No.4</p>
+      <p className="infos-block">
+        Il préfère les endroits chauds. En cas de pluie, de la vapeur se forme
+        autour de sa queue. NIV.10 No.4
+      </p>
     </Container>
   );
 };
